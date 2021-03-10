52ac3586a3bf3d7a672818f730811a315c7595ec
John Grinwis <john@grinwis.com>
2021-03-10 15:28:25 +0100

    clean up some stuff

--- __main__.py
+++ __main__.py
@@ -3,11 +3,11 @@
 import pulumi
 import pulumi_akamai as akamai
 
-# should use ENV to share between stacks
+# as a test set some static vars which could be shared between stacks.
 group_name = "GSS Training Internal-C-1IE2OHM"
 cpcode_name = "jgrinwis-pulumi"
 
-# we can export it or re-query it again
+# we can export it or re-query it again in other stack
 contract_id = akamai.get_contracts().contracts[0].contract_id
 group_id = akamai.get_group(contract_id=contract_id, group_name=group_name).id
 
@@ -33,6 +33,7 @@ product_id = products[product]
 
 # create cpcode and using name as the export value in the next stack
 # https://www.pulumi.com/docs/reference/pkg/akamai/cpcode/
+# you can create a new cpcode but you can't remove it.
 cpcode = akamai.CpCode(
     cpcode_name,
     contract_id=contract_id,
