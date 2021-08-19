db18cb660bc1b9995554d2d4f82f621ac71d24a4
John Grinwis <john.grinwis@gmail.com>
2021-08-19 16:15:30 +0200

    pulumi version

--- __main__.py
+++ __main__.py
@@ -3,9 +3,10 @@
 import pulumi
 import pulumi_akamai as akamai
 
-# get information from our config file. 
+# get information from our config file.
 # use "pulumi config set <key> [value]" to set the value
-# Config is unique per project/stack. 
+# make sure to select correct .edgerc section via $ pulumi config set akamai:propertySection <section>
+# Config is unique per project/stack.
 # To create a new stack in a project use "pulumi stack init" from project dir and select it via "pulumi stack select"
 config = pulumi.Config()
 group_name = config.require("group_name")
@@ -21,8 +22,8 @@ group_id = akamai.get_group(contract_id=contract_id, group_name=group_name).id
 
 # check products available on contract via
 # first list contracts via: akamai pm lc
-# lookup product on contract via : akamai pm lp -c <contract_id> 
-product = 'dsa'
+# lookup product on contract via : akamai pm lp -c <contract_id>
+product = 'ion'
 
 # engineering code names don't match product names
 # below a list of products with their engineering names
@@ -35,7 +36,7 @@ products = {
 }
 
 # define the product id you want to use for your property
-# this should match the template and cpcode 
+# this should match the template and cpcode
 # https://registry.terraform.io/providers/akamai/akamai/latest/docs/guides/appendix
 product_id = products[product]
 
@@ -43,16 +44,16 @@ product_id = products[product]
 # https://www.pulumi.com/docs/reference/pkg/akamai/cpcode/
 # you can create a new cpcode but you can't remove it.
 
-# cpcode = akamai.CpCode(
-#     cpcode_name,
-#     contract_id=contract_id,
-#     group_id=group_id,
-#     name=cpcode_name,
-#     product=product_id
-# )
+cpcode = akamai.CpCode(
+    cpcode_name,
+    contract_id=contract_id,
+    group_id=group_id,
+    name=cpcode_name,
+    product=product_id
+)
 
-cpcode = "jgrinwis-pristine"
+# cpcode = "jgrinwis-pristine"
 
 # share created vars with other stack so we don't need to recreate them
 #pulumi.export('cpcode_name', cpcode.name)
-pulumi.export('cpcode_name', cpcode)
+pulumi.export('cpcode_name', cpcode.name)
